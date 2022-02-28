90945bad513343fc9ecdce2f97596363b9f56f7c
Katon Zaky <zakykaton@gmail.com>
2022-02-27 19:49:27 -0600

    updates

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -1,12 +1,13 @@
 import { Subjects } from './subjects';
 
 export interface TicketUpdatedEvent {
-  subject: Subjects.TicketUpdated;
-  data: {
-    id: string;
-    version: number;
-    title: string;
-    price: number;
-    userId: string;
-  };
+	subject: Subjects.TicketUpdated;
+	data: {
+		id: string;
+		version: number;
+		title: string;
+		price: number;
+		userId: string;
+		orderId: string;
+	};
 }
